3421ec36051b106d062229c88747afb0b599a4d3
danniBr <danitza.br@gmail.com>
2018-08-02 21:34:27 -0400

    nabvar

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,7 +10,7 @@ class EmployeesController < ApplicationController
   def destroy
   	@employee = Employee.find(params[:id])
     @employee.destroy
-    redirect_to companies_path
+    redirect_to company_path(@employee.company_id)
   end
 
   private
